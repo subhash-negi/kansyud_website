371bb83b666b9f99908c63af845574c32e5d9320
subhash-negi <81283313+subhash-negi@users.noreply.github.com>
2023-02-01 18:09:46 +0530

    Update commands.js

--- src/components/commands.js
+++ src/components/commands.js
@@ -4,7 +4,7 @@ function commanings(props){
     return(
         <div>
             <div className='flex-col'>
-                <img className='h-18 w-16 ml-4 mt-6 rounded-full border-purple-500 shadow-md shadow-black border-2 object-cover ' src={props.image}></img>
+                <img className='h-18 w-16 ml-4 mt-6 rounded-full border-purple-500 border-2 object-cover ' src={props.image}></img>
                 <p className=' ml-2 text-left text-xs'>{props.name}</p>
                 <p className=' ml-2 text-left text-xs'>{props.desig}</p>
             </div>
